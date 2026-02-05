f0c426b2e37132c1cb51c4f30f2c4974c48b385b
amyward555777 <amyward62@outlook.com>
2026-02-05 10:18:18 -0500

    Add IPlayer interface and PlayerBase abstract class

--- CrazyEights/Player/IPlayer.cs
+++ CrazyEights/Player/IPlayer.cs
@@ -1,6 +1,14 @@
+using CrazyEights.Cards;
+using CrazyEights.Game;
+
 namespace CrazyEights.Player;
 
 public interface IPlayer
 {
+    string Name { get; }
+    int CardCount { get; }
     
+    void DrawCard(ICard card);
+    
+    void TakeTurn(TurnContext context);
 }
--- CrazyEights/Player/PlayerBase.cs
+++ CrazyEights/Player/PlayerBase.cs
@@ -1,6 +1,54 @@
+using CrazyEights.Cards;
+using CrazyEights.Domain;
+using CrazyEights.Game;
+
 namespace CrazyEights.Player;
 
-public class PlayerBase
+public abstract class PlayerBase : IPlayer
 {
-    
+    private readonly List<ICard> _hand = new();
+
+    public string Name { get; }
+    public int CardCount => _hand.Count;
+
+    protected PlayerBase(string name)
+    {
+        Name = name;
+    }
+
+    public void DrawCard(ICard card)
+    {
+        _hand.Add(card);
+    }
+
+    protected ICard PlayCardAtIndex(int index)
+    {
+        ICard card = _hand[index];
+        _hand.RemoveAt(index);
+        return card;
+    }
+
+    public IReadOnlyList<ICard> PeekHand()
+    {
+        return _hand.AsReadOnly();
+    }
+
+    protected List<ICard> FindPlayable(ICard topDiscard, Suit suitToMatch)
+    {
+        var playable = new List<ICard>();
+        foreach (var card in _hand)
+        {
+            if (card.IsPlayable(topDiscard, suitToMatch))
+                playable.Add(card);
+        }
+        
+        return playable;
+    }
+
+    protected void RemoveFromHand(ICard card)
+    {
+        _hand.Remove(card);
+    }
+
+    public abstract void TakeTurn(TurnContext context);
 }
