aa1e19f35af6d2daeb5e1ce55b8a7c9128149663
amyward555777 <amyward62@outlook.com>
2026-02-05 09:45:08 -0500

    Add Deck and DiscardPile implementation

--- CrazyEights/CardDeck/Deck.cs
+++ CrazyEights/CardDeck/Deck.cs
@@ -1,6 +1,42 @@
+using CrazyEights.Cards;
+using CrazyEights.Domain;
+
 namespace CrazyEights.CardDeck;
 
 public class Deck
 {
+    private readonly Stack<ICard> _cards;
+
+    private Deck(IEnumerable<ICard> cards)
+    {
+        _cards = new Stack<ICard>(cards);
+    }
     
+    public int Count => _cards.Count;
+
+    public ICard? Draw()
+    {
+        if (_cards.Count == 0)
+            return null;
+        
+        return _cards.Pop();
+    }
+
+    // Fisher-Yates shuffle
+    public static Deck Shuffle(Random rng)
+    {
+        var cards = new List<ICard>(52);
+
+        foreach (Suit suit in Enum.GetValues<Suit>())
+            foreach (Rank rank in Enum.GetValues<Rank>())
+                cards.Add(new StandardCard(rank, suit));
+
+        for (int i = cards.Count - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            (cards[i], cards[j]) = (cards[j], cards[i]);
+        }
+
+        return new Deck(cards);
+    }
 }
--- CrazyEights/CardDeck/DiscardPile.cs
+++ CrazyEights/CardDeck/DiscardPile.cs
@@ -1,6 +1,15 @@
+using CrazyEights.Cards;
+
 namespace CrazyEights.CardDeck;
 
 public class DiscardPile
 {
-    
+    private readonly Stack<ICard> _pile = new Stack<ICard>();
+
+    public void Push(ICard card)
+    {
+        _pile.Push(card); 
+    }
+
+    public ICard Top => _pile.Peek();
 }
