e6c79430ec458bcd74d1dbb8b450c92bb7f2dace
amyward555777 <amyward62@outlook.com>
2026-02-05 11:07:18 -0500

    Add TurnContext and TurnAction implementation

--- CrazyEights/Game/TurnAction.cs
+++ CrazyEights/Game/TurnAction.cs
@@ -1,6 +1,26 @@
+using CrazyEights.Cards;
+using CrazyEights.Domain;
+
 namespace CrazyEights.Game;
 
 public class TurnAction
 {
+    private TurnAction(ICard? cardToPlay, bool drewCard, Suit? declaredSuit)
+    {
+        CardToPlay = cardToPlay;
+        DrewCard = drewCard;
+        DeclaredSuit = declaredSuit;
+    }
     
+    public ICard? CardToPlay { get; }
+    public bool DrewCard { get; }  
+    public Suit? DeclaredSuit { get; }
+    
+    public static TurnAction EndOfTurnAfterDraw() => new(cardToPlay: null, drewCard: true, declaredSuit: null);
+    
+    public static TurnAction Play(ICard card, Suit? declaredSuit = null) =>
+        new(cardToPlay: card, drewCard: false, declaredSuit: declaredSuit);
+    
+    public static TurnAction DrawAndPlay(ICard card, Suit? declaredSuit = null) =>
+        new(cardToPlay: card, drewCard: true, declaredSuit: declaredSuit);
 }
--- CrazyEights/Game/TurnContext.cs
+++ CrazyEights/Game/TurnContext.cs
@@ -1,6 +1,44 @@
+using CrazyEights.Cards;
+using CrazyEights.Domain;
+
 namespace CrazyEights.Game;
 
 public class TurnContext
 {
-    
+    public ICard TopDiscard { get; }
+    public Suit SuitToMatch { get; }
+    public int RoundNumber { get; }
+
+    private readonly Func<ICard?> _drawCard;
+    private readonly Action<ICard> _playToDiscard;
+    private readonly Action<Suit> _setSuit;
+
+    public TurnContext(
+        ICard topDiscard,
+        Suit suitToMatch,
+        int roundNumber,
+        Func<ICard?> drawCard,
+        Action<ICard> playToDiscard,
+        Action<Suit> setSuit)
+    {
+        TopDiscard = topDiscard;
+        SuitToMatch = suitToMatch;
+        RoundNumber = roundNumber;
+        
+        _drawCard = drawCard;
+        _playToDiscard = playToDiscard;
+        _setSuit = setSuit;
+    }
+
+    public ICard? DrawCard() => _drawCard();
+
+    public void PlayCard(ICard card)
+    {
+        _playToDiscard(card);
+    }
+
+    public void DeclareSuit(Suit suit)
+    {
+        _setSuit(suit);
+    }
 }
